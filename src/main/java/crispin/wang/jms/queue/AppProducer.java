d7d0965569cdf701c88974ef04d5af5535f7bea6
Chris <chriswang.coder@gmail.com>
2018-06-22 22:20:52 +0800

    first commit

---
+++ src/main/java/crispin/wang/jms/queue/AppProducer.java
@@ -0,0 +1,26 @@
+package crispin.wang.jms.queue;
+
+/**
+ * 消息提供者
+ *
+ * @author : chris
+ * 2018-06-22
+ */
+public class AppProducer {
+    private static final String URL = "tcp://101.132.47.22:61616";
+    private static final String QUEUE_NAME = "queue-test";
+
+    public static void main(String[] args) {
+        // 1.创建ConnectionFactory
+
+        // 2.创建Connection
+
+        // 3.启动连接
+
+        // 4.创建会话
+
+        // 5.创建一个目标
+
+
+    }
+}
